412b4fa65d2af8c52965ed922b47d9dcf3d35a33
Alexey Smirnov <asmirnov@virgilsecurity.com>
2020-08-20 19:44:28 +0300

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    semi: ["warn", "always"],
+    "react/prop-types": "off",
+  },
+};
