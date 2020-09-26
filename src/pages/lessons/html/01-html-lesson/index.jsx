477e4e705ff1fa7330b487946b4dbc437be23cf9
Alexey Smirnov <asmirnov@virgilsecurity.com>
2020-09-26 18:14:00 +0300

    add definitions

---
+++ src/pages/lessons/html/01-html-lesson/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import LessonTemplate from "../../../../templates/lesson-template";
+import htmlLogo from "./html-logo.png";
+import styled from "styled-components";
+import markupDefinition from "raw-loader!./markup-definition.html";
+
+const Logo = styled.img`
+  height: 30vh;
+  width: auto;
+`;
+
+export default function FirstLesson() {
+  return (
+    <LessonTemplate>
+      <div className="slides">
+        <section>
+          <h3>Урок 1. </h3>
+          <h1>Героическое вступление в</h1>
+          <Logo src={htmlLogo} />
+        </section>
+        <section>
+          <h2>Основные понятия</h2>
+          <section>
+            <p className="frament">
+              <b>HTML</b>{" "}
+              <i>
+                (от англ. HyperText Markup Language — «язык гипертекстовой
+                разметки»)
+              </i>{" "}
+              — стандартизированный язык разметки веб-страниц во Всемирной
+              паутине (интернете)
+            </p>
+            <p className="fragment">(c) Wikipedia</p>
+            <p className="fragment">
+              <b>Ваши мысли в этот момент: </b>
+              <i>
+                Ну и, я и сам могу это в википедии прочитать, что это вообще
+                значит, умник?
+              </i>
+            </p>
+          </section>
+          <section>
+            <b>Гипертекст</b> <i>(англ. HyperText)</i> - текст, отличающийся от обычного тем, что
+            содержит{" "}
+            <a
+              href="https://www.w3.org/WhatIs.html#:~:text=Hypertext%20is%20text%20which%20contains,to%20use%20this%20term%20too."
+              target="_blank"
+              rel="noreferrer"
+            >
+              гиперссылки
+            </a>{" "}
+            (далее просто ссылки), т.е можно из одного текста перейти в другой
+            текст по ссылке.
+          </section>
+          <section>
+            <p dangerouslySetInnerHTML={{ __html: markupDefinition }} />
+            <p className="fragment">
+              <pre>
+                <code data-trim data-noescape data-line-numbers>
+                  {markupDefinition}
+                </code>
+              </pre>
+            </p>
+          </section>
+        </section>
+      </div>
+    </LessonTemplate>
+  );
+}
--- src/templates/lesson-template.jsx
+++ src/templates/lesson-template.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 import styled from "styled-components";
 
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/reset.css";
+import "../lib/reveal-theme/atom-one-dark.css";
 import "../lib/reveal-theme/source/hipsta.scss";
 
 const RevealViewport = styled.div`
@@ -16,7 +17,7 @@ const LessonTemplate = ({ children }) => {
     const deck = new Reveal({
       overview: true,
       mouseWheel: true,
-      plugins: [Markdown],
+      plugins: [Markdown, Highlight],
     });
     deck.initialize();
   }, []);
