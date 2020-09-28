69bf7c6bee35858aacb7cb01f507fb00d44759b5
Alexey Smirnov <asmirnov@virgilsecurity.com>
2020-09-29 00:39:07 +0300

    add aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,9 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     devtool: "eval-source-map",
+    resolve: {
+      alias: {
+        lib: path.resolve(__dirname, "src/lib"),
+        components: path.resolve(__dirname, "src/components"),
+        templates: path.resolve(__dirname, "src/templates"),
+        images: path.resolve(__dirname, "src/images"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+        assets: path.resolve(__dirname, "src/assets"),
+        pages: path.resolve(__dirname, "src/pages"),
+      },
+    },
   });
 };
