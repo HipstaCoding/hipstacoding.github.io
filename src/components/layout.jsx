28cdd81ef95a4254923b428054f08d486bbbf13d
Alexey Smirnov <asmirnov@virgilsecurity.com>
2020-10-22 15:16:26 +0300

    update to react 17.0.0 and gatsby 2.24.85

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import "assets/imports.css";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
-	@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap');
-
 	body,html {
     margin: 0;
     padding: 0;
 	}
+
 	* {
 			box-sizing: border-box;
 	}
 `;
 
 const Container = styled.div`
-	overflow: hidden;
-`
+  overflow: hidden;
+`;
 
 const Layout = ({ children }) => (
   <Container>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-import OnDeviceScreen from "../components/onDeviceScreen";
+// import OnDeviceScreen from "../components/onDeviceScreen";
 import Layout from "../components/layout";
 import Lewkanfun from "../lewkanfun/App";
 
 const IndexPage = () => {
   return (
     <Layout>
-      <OnDeviceScreen>
-        <Lewkanfun />
-      </OnDeviceScreen>
+      {/* <OnDeviceScreen> */}
+      <Lewkanfun />
+      {/* </OnDeviceScreen> */}
     </Layout>
   );
 };
-
-// export const pageQuery = graphql`
-//   query IndexQuery {
-//     macbook: file(relativePath: { eq: "macbook_bg.png" }) {
-//       childImageSharp {
-//         fluid(quality: 100, maxWidth: 1980, maxHeight: 1173) {
-//           ...GatsbyImageSharpFluid_withWebp
-//         }
-//       }
-//     }
-//   }
-// `;
 
 export default IndexPage;
