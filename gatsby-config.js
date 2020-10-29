741bed2ce532371a9592ffc644c5d147b3de3bf7
Alexey Smirnov <xlwknx@gmail.com>
2020-10-29 17:30:01 +0200

    add robots txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-svgr",
     "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
