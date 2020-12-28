ebf2ef21e12c0aca2244dd7350301e089657193a
Alexey Smirnov <xlwknx@gmail.com>
2020-12-28 18:21:03 +0200

    position: relative

--- src/components/lessons/mdx/InlineCode.jsx
+++ src/components/lessons/mdx/InlineCode.jsx
@@ -2,13 +2,12 @@ import styled from "styled-components";
 
 const StyledInlineCode = styled.code`
   background-color: ${({ theme }) => theme.colors.backgroundSecondary};
-  display: inline-block;
-  /* line-height: 28px; */
-  color: ${({ theme }) => theme.colors.textColor};
-  border: 0.05em solid ${({ theme }) => theme.colors.textColor};
-  border-radius: 3px;
+  /* display: inline-block; */
+  /* color: ${({ theme }) => theme.colors.green}; */
+  /* border: 1px solid ${({ theme }) => theme.colors.backgroundPrimary}; */
+  border-radius: 0.1em;
   padding: 0 0.2em;
-  vertical-align: bottom;
+  /* vertical-align: bottom; */
 `;
 
 export default function InlineCode(props) {
