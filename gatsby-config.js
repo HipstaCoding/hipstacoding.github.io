2bc6babefe8fc4fa5bd478838e733f04e304b7fe
Alexey Smirnov <xlwknx@gmail.com>
2021-02-09 15:38:33 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,18 +98,18 @@ module.exports = {
     },
     "gatsby-plugin-sass",
     "gatsby-plugin-sitemap",
-    // {
-    //   resolve: "gatsby-plugin-manifest",
-    //   options: {
-    //     name: title,
-    //     short_name: "hipstacoding.school",
-    //     start_url: "/",
-    //     background_color: "#282c34",
-    //     display: "minimal-ui",
-    //     icon: "src/assets/images/hipsta-logo-favicon.png", // This path is relative to the root of  the site.
-    //     theme_color_in_head: false,
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: title,
+        short_name: "hipstacoding.school",
+        start_url: "/",
+        background_color: "#282c34",
+        display: "minimal-ui",
+        icon: "src/assets/icons/favicon.png", // This path is relative to the root of  the site.
+        theme_color_in_head: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // "gatsby-plugin-offline",
